b9c26882c507162ee1bcfdb6dd80364e1ba7c552
gkuenzi <gavin.kuenzi@coyotes.usd.edu>
2026-03-12 00:14:49 +0000

    Pivot to React for Ease of Components, Implemented basic opVote component and rough draft design

---
+++ r6-ranker/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+import OperatorVote from './assets/components/OperatorVote'
+
+function App() {
+
+  return (
+    <div>
+      <OperatorVote />
+    </div>
+  )
+}
+
+export default App
---
+++ r6-ranker/src/assets/components/OperatorVote.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import './OpVote.css'
+
+function operatorVote() {
+
+  return (
+    <div>
+      <div className="mainContainer">
+        <h1>Scenario/Site</h1>
+        <div className="opButtons">
+          <button>Operator A</button>
+          <button>Operator B</button>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default operatorVote
