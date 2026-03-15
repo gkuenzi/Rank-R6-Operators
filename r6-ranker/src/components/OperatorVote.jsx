4bcb0cf46cb0174d8ff6adf64212769f6427c457
gkuenzi <gavin.kuenzi@coyotes.usd.edu>
2026-03-15 21:10:00 +0000

    randomized map image and situation display on operator click

--- r6-ranker/src/components/OperatorVote.jsx
+++ r6-ranker/src/components/OperatorVote.jsx
@@ -2,8 +2,24 @@ import { useState, useEffect } from 'react'
 import './OpVote.css'
 import attackersData from '../../../Attackers.txt'
 import defendersData from '../../../Defenders.txt'
-import mapImage from '../assets/placeholder-cover-photo.jpg'
 import r6Logo from '../assets/simple-r6-logo.png'
+import placeholderCover from '../assets/placeholder-cover-photo.jpg'
+import bankCover from '../assets/bank-cover-photo.avif'
+import borderCover from '../assets/border-cover-photo.avif'
+import chaletCover from '../assets/chalet-cover-photo.avif'
+import clubhouseCover from '../assets/clubhouse-cover-photo.avif'
+import coastlineCover from '../assets/coastline-cover-photo.avif'
+import consulateCover from '../assets/consulate-cover-photo.avif'
+import emeraldCover from '../assets/emerald-cover-photo.avif'
+import fortressCover from '../assets/fortress-cover-photo.avif'
+import kafeCover from '../assets/kafe-cover-photo.avif'
+import lairCover from '../assets/lair-cover-photo.avif'
+import nighthavenCover from '../assets/nighthaven-cover-photo.avif'
+import oregonCover from '../assets/oregon-cover-photo.avif'
+import outbackCover from '../assets/outback-cover-photo.avif'
+import skyscraperCover from '../assets/skyscraper-cover-photo.avif'
+import themeparkCover from '../assets/themepark-cover-photo.avif'
+import villaCover from '../assets/villa-cover-photo.avif'
 
 const maps = [
   'bank',
@@ -24,7 +40,26 @@ const maps = [
   'villa',
 ];
 
-function opButton(operatorSide, setA, setB) {
+const mapImages = {
+  bank: bankCover,
+  border: borderCover,
+  chalet: chaletCover,
+  clubhouse: clubhouseCover,
+  coastline: coastlineCover,
+  consulate: consulateCover,
+  emerald: emeraldCover,
+  fortress: fortressCover,
+  kafe: kafeCover,
+  lair: lairCover,
+  nighthaven: nighthavenCover,
+  oregon: oregonCover,
+  outback: outbackCover,
+  skyscraper: skyscraperCover,
+  themepark: themeparkCover,
+  villa: villaCover,
+};
+
+function opButton(operatorSide, setA, setB, setSit, setMapImage) {
   const randomIndexA = Math.floor(Math.random() * operatorSide.length)
   setA(operatorSide[randomIndexA])
 
@@ -35,6 +70,11 @@ function opButton(operatorSide, setA, setB) {
       break;
     }
   }
+
+  const randMap = Math.floor(Math.random() * maps.length)
+  const selectedMap = maps[randMap]
+  setSit(selectedMap)
+  setMapImage(mapImages[selectedMap])
 }
 
 export default function operatorVote() {
@@ -42,6 +82,8 @@ export default function operatorVote() {
   const [operatorB, setOperatorB] = useState('Defense')
   const [attackers, setAttackers] = useState([])
   const [defenders, setDefenders] = useState([])
+  const [situation, setSituation] = useState('Choose your Team')
+  const [currentMapImage, setCurrentMapImage] = useState(placeholderCover)
 
   const atk = true; const def = false;
   const [team, setTeam] = useState();
@@ -74,27 +116,27 @@ export default function operatorVote() {
         <h1>Operators-Ranked</h1>
       </div>
       <div className="mainContainer">
-        <img src={mapImage} alt="Chalet Basement Blueprint" className="mapImage" />
+        <img src={currentMapImage} alt="Chalet Basement Blueprint" className="mapImage" />
         <div className="userContainer">
-          <h1>Choose a Side</h1>
+        <h1>{situation}</h1>
           <div className="opButtons">
             <button onClick={() => {
-              if (team === atk) opButton(attackers, setOperatorA, setOperatorB)
-              else if (team === def) opButton(defenders, setOperatorA, setOperatorB)
+              if (team === atk) opButton(attackers, setOperatorA, setOperatorB, setSituation, setCurrentMapImage)
+              else if (team === def) opButton(defenders, setOperatorA, setOperatorB, setSituation, setCurrentMapImage)
               else {
                 setTeam(atk)
-                opButton(attackers, setOperatorA, setOperatorB)
+                opButton(attackers, setOperatorA, setOperatorB, setSituation, setCurrentMapImage)
               }
 
             }}
             >{operatorA}
             </button>
             <button onClick={() => {
-              if (team === atk) opButton(attackers, setOperatorA, setOperatorB)
-              else if (team === def) opButton(defenders, setOperatorA, setOperatorB)
+              if (team === atk) opButton(attackers, setOperatorA, setOperatorB, setSituation, setCurrentMapImage)
+              else if (team === def) opButton(defenders, setOperatorA, setOperatorB, setSituation, setCurrentMapImage)
               else {
                 setTeam(def)
-                opButton(defenders, setOperatorA, setOperatorB)
+                opButton(defenders, setOperatorA, setOperatorB, setSituation, setCurrentMapImage)
               }
             }}
             >{operatorB}
