01733f0782b257871c5fafccf618f319a2c451b5
gkuenzi <gavin.kuenzi@coyotes.usd.edu>
2026-04-08 16:32:15 +0000

    create a situation point scale

---
+++ r6-ranker/src/components/situationScale.jsx
@@ -0,0 +1,27 @@
+
+
+
+export function situationScale() {
+    const scale = [
+        {
+            situation: '',
+            general: 0,
+            support: 0,
+            fragging: 0,
+            intel: 0,
+            entry: 0,
+            roam: 0,
+            vert: 0
+        },
+        {
+            situation: '',
+            general: 0,
+            support: 0,
+            fragging: 0,
+            intel: 0,
+            entry: 0,
+            roam: 0,
+            vert: 0
+        },
+    ]
+}
