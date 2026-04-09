663c81644c76ac27fb1b8dfd751f413b8e86a999
gkuenzi <gavin.kuenzi@coyotes.usd.edu>
2026-04-09 02:06:36 +0000

    increased number of situations

--- r6-ranker/src/components/situationScale.jsx
+++ r6-ranker/src/components/situationScale.jsx
@@ -6,7 +6,7 @@ export function situationScale() {
             // ---- All ----
     {
         type: 'all',
-        situation: 'gain intel about the opposing team',
+        situation: 'Gain intel about the opposing team',
         general: 10,
         support: 10,
         fragging: 0,
@@ -20,23 +20,46 @@ export function situationScale() {
         situation: 'Deny plant/diffuse from floor above or below',
         general: 10,
         support: 0,
-        fragging: 0,
-        intel: 0,
+        fragging: 3,
+        intel: 3,
         entry: 0,
         roam: 10,
         vert: 10
     },
     {
         type: 'all',
-        situation: 'fragging (getting kills)',
+        situation: 'Fragging (Getting kills)',
         general: 10,
         support: 0,
         fragging: 10,
         intel: 0,
         entry: 0,
+        roam: 3,
+        vert: 0
+    },
+    {
+        type: 'all',
+        situation: 'Get rid of enemy utility',
+        general: 10,
+        support: 5,
+        fragging: 1,
+        intel: 7,
+        entry: 6,
         roam: 0,
         vert: 0
     },
+    {
+        type: 'all',
+        situation: 'Clutch a 1v5',
+        general: 10,
+        support: 2,
+        fragging: 10,
+        intel: 4,
+        entry: 3,
+        roam: 0,
+        vert: 1
+    },
+
 
     // ---- Attack ----
     {
@@ -52,7 +75,7 @@ export function situationScale() {
     },
     {
         type: 'attack',
-        situation: 'taking space vertically on the map (above or below sight)',
+        situation: 'Taking space vertically on the map (above or below sight)',
         general: 10,
         support: 5,
         fragging: 0,
@@ -63,10 +86,10 @@ export function situationScale() {
     },
     {
         type: 'attack',
-        situation: 'take space off-site; take control of rooms',
+        situation: 'Take space off-site; take control of rooms',
         general: 10,
-        support: 0,
-        fragging: 0,
+        support: 3,
+        fragging: 3,
         intel: 0,
         entry: 10,
         roam: 10,
@@ -80,36 +103,36 @@ export function situationScale() {
         general: 10,
         support: 0,
         fragging: 10,
-        intel: 0,
+        intel: 3,
         entry: 0,
         roam: 10,
-        vert: 0
+        vert: 3
     },
     {
         type: 'defense',
-        situation: 'post-plant situation',
+        situation: 'Post-plant situation',
         general: 10,
         support: 0,
         fragging: 5,
-        intel: 0,
+        intel: 5,
         entry: 0,
         roam: 0,
-        vert: 0
+        vert: 2
     },
     {
         type: 'defense',
-        situation: 'set up sight and deny enemy utility',
+        situation: 'Set up sight and deny enemy utility',
         general: 10,
-        support: 0,
-        fragging: 0,
-        intel: 0,
+        support: 10,
+        fragging: 2,
+        intel: 6,
         entry: 0,
         roam: 0,
         vert: 0
     },
     {
         type: 'defense',
-        situation: 'flexible; can be both and anchor and roamer',
+        situation: 'Flexible; Can be both and anchor and roamer',
         general: 10,
         support: 5,
         fragging: 5,
@@ -127,8 +150,19 @@ export function situationScale() {
         intel: 5,
         entry: 0,
         roam: 0,
+        vert: 2
+    },
+    {
+        type: 'defense',
+        situation: 'Delay attackers / Waste time without taking fights',
+        general: 10,
+        support: 8,
+        fragging: 2,
+        intel: 5,
+        entry: 0,
+        roam: 10,
         vert: 0
-    }
+    },
     ]
 
     return scale;
